0405baf72474c258eb90f1a028c46e5f0cfc0ff9
Mason Bassett <bassettmason@gmail.com>
2023-08-15 18:31:52 -0700

    integration test fix

--- api/tests/integration/test_routes.py
+++ api/tests/integration/test_routes.py
@@ -1,10 +1,8 @@
-# from fastapi.testclient import TestClient
-# from api.main import app
-# client = TestClient(app)
+from fastapi.testclient import TestClient
+from fastapi import status
+from api.main import app
+client = TestClient(app=app)
 
-# def test_scrape_flixlist_route():
-#     # response = client.get("/api/scrape/flixlist/")
-#     # assert response.status_code == 200
-#     # assert response.json() == {"message": "Scraped data from flixlist"}
-#     hey1 = 0
-#     assert hey1 == 0
+def test_scrape_flixlist_route():
+    response = client.get("/api/scrape/flixlist/")
+    assert response.status_code == status.HTTP_200_OK
