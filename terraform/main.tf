bb99d612e55d5bac3c44ce2ce77dad25277a1448
Mason Bassett <bassettmason@gmail.com>
2023-08-20 13:03:59 -0700

    main tf changes and cicd changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,23 @@
 provider "google" {
   credentials = file(var.gcp_credentials_file)
-  project     = var.project_id
-  region      = var.region
+  project     = "media-djinn"
+  region      = "us-west1"
 }
 
 resource "google_artifact_registry_repository" "default" {
-  location      = var.region
-  repository_id = var.repository_id
+  location      = "us-west1"
+  repository_id = "media-djinn"
   format        = "DOCKER"
 }
 
 resource "google_cloud_run_service" "default" {
   name     = var.service_name
-  location = var.region
+  location = "us-west1"
 
   template {
     spec {
       containers {
-        image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.default.repository_id}/${var.image_name}"
+        image = "us-west1-docker.pkg.dev/media-djinn/media-djinn-registry/${var.image_name}"
       }
     }
   }
