7eac493f5ca800deecea9d0f3d6857beb1480261
Mason Bassett <bassettmason@gmail.com>
2023-08-20 13:53:22 -0700

    image only no deploy test

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,30 +6,20 @@ provider "google" {
 
 resource "google_artifact_registry_repository" "default" {
   location      = "us-west1"
-  repository_id = "media-djinn"
+  repository_id = "media-djinn-registry"
   format        = "DOCKER"
 }
 
-resource "google_cloud_run_service" "default" {
-  name     = var.service_name
-  location = "us-west1"
+  # template {
+  #   spec {
+  #     service_account_name = "ci-cd-service-account@media-djinn.iam.gserviceaccount.com"
 
-  template {
-    spec {
-      service_account_name = "ci-cd-service-account@media-djinn.iam.gserviceaccount.com"
+  #     containers {
+  #       image = "us-west1-docker.pkg.dev/media-djinn/media-djinn-registry/${var.image_name}"
+  #     }
+  #   }
+  # }
 
-      containers {
-        image = "us-west1-docker.pkg.dev/media-djinn/media-djinn-registry/${var.image_name}"
-      }
-    }
-  }
 
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
 
-output "service_url" {
-  value = google_cloud_run_service.default.status[0].url
-}
+
