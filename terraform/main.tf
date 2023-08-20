9bed7199bf4ac6ce45bad87d654f9746d5214d61
Mason Bassett <bassettmason@gmail.com>
2023-08-20 14:18:20 -0700

    back to registry only

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,26 +10,25 @@ resource "google_artifact_registry_repository" "default" {
   format        = "DOCKER"
 }
 
-resource "google_cloud_run_service" "default" {
-  name     = var.service_name
-  location = "us-west1"
 
-  template {
-    spec {
-      service_account_name = "ci-cd-service-account@media-djinn.iam.gserviceaccount.com"
+# resource "google_cloud_run_service" "default" {
+#   name     = var.service_name
+#   location = "us-west1"
 
-      containers {
-        image = "us-west1-docker.pkg.dev/media-djinn/media-djinn-registry/flix-fire-scraper-image:latest"
-      }
-    }
-  }
+#   template {
+#     spec {
+#       containers {
+#         image = "us-west1-docker.pkg.dev/media-djinn/media-djinn-registry/${var.image_name}"
+#       }
+#     }
+#   }
 
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+# }
 
-output "service_url" {
-  value = google_cloud_run_service.default.status[0].url
-}
+# output "service_url" {
+#   value = google_cloud_run_service.default.status[0].url
+# }
