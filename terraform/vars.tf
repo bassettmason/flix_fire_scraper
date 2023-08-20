d9ec7ceeff4f8fc287eb038eb99c020cb1a03b3f
Mason Bassett <bassettmason@gmail.com>
2023-08-20 11:27:23 -0700

    terraform changes

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,28 +1,29 @@
 variable "gcp_credentials_file" {
   description = "Path to the GCP credentials JSON file"
-  default     = "./credentials.json"  # you can adjust this path
+  default     = "./credentials.json"
 }
 
 variable "project_id" {
-  description = "The ID of the project in GCP"
+  description = "GCP project ID"
+  default     = "media-djinn"
 }
 
 variable "region" {
   description = "The region to deploy resources in"
-  default     = "us-central1"  # default to US central
+  default     = "us-west1"
 }
 
 variable "service_name" {
   description = "The name of the Cloud Run service"
-  default     = "my-fastapi-service"
+  default     = "flix-fire-scraper-api"
 }
 
 variable "image_name" {
   description = "Name of the Docker image in GAR"
-  default     = "my-fastapi-image:latest"
+  default     = "flix-fire-scraper:latest" 
 }
 
 variable "repository_id" {
   description = "ID of the Artifact Registry repository"
-  default     = "my-repo-name"  # Replace with your actual repository name or remove the default if you want to provide it dynamically
+  default     = "media-djinn-registry" 
 }
